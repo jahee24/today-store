de6adf8a6425ecaa65df0d57163be82144108457
Talker456 <103106936+Talker456@users.noreply.github.com>
2026-02-24 19:30:56 +0900

    chore(backend) : 프로젝트 초기 세팅 (#1)
    [Backend] 초기 프로젝트 세팅
    - spring boot 프로젝트 생성
    - 의존성 설정
    - 패키기 구조 생성

---
+++ backend-spring/today-store/src/test/java/today_store/TodayStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package today_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
