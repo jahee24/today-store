dadcb95cc430bdcdf51539970fdd84af56a9f7ac
KimMinWoo <melonmozzi2@naver.com>
2026-03-09 16:26:55 +0900

    test: backend 테스트 환경 분리 및 GCS mock 설정 추가

--- backend-spring/today-store/src/test/java/today_store/TodayStoreApplicationTests.java
+++ backend-spring/today-store/src/test/java/today_store/TodayStoreApplicationTests.java
@@ -1,10 +1,15 @@
 package today_store;
 
+import com.google.cloud.storage.Storage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class TodayStoreApplicationTests {
+
+	@MockitoBean
+	private Storage storage;
 
 	@Test
 	void contextLoads() {
