7943f2b70811f4edc23f735b799e4101da338ff3
Talker456 <ttppzz406@naver.com>
2026-03-09 20:57:49 +0900

    feature(backend) : Oauth2 User info 추상화
    - oauth2 provider에서 제공받은 정보를 커스텀 클래스로 추상화
    - kakao, google에 따라 factory 클래스를 통해 oauth2 user 정보 관리

---
+++ backend-spring/today-store/src/main/java/today_store/authentication/oauth2/OAuth2UserInfoFactory.java
@@ -0,0 +1,15 @@
+package today_store.authentication.oauth2;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        if (registrationId.equalsIgnoreCase("google")) {
+            return new GoogleOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase("kakao")) {
+            return new KakaoOAuth2UserInfo(attributes);
+        } else {
+            throw new IllegalArgumentException("Invalid Provider Type.");
+        }
+    }
+}
